c9f432124dfcc85a4f691bd61a9aee738c28f14f
leandro <lgomesb@gmail.com>
2024-01-21 23:01:08 +0000

    add project

--- product-integration-service/src/main/java/com/barbosa/ms/productintegrationservice/productintegrationservice/services/ProductIntegrationServiceService.java
+++
@@ -1,7 +0,0 @@
-package com.barbosa.ms.productintegrationservice.productintegrationservice.services;
-
-import com.barbosa.ms.productintegrationservice.productintegrationservice.domain.records.ProductIntegrationServiceRecord;
-
-public interface ProductIntegrationServiceService extends DomainService<ProductIntegrationServiceRecord>{
-    
-}
