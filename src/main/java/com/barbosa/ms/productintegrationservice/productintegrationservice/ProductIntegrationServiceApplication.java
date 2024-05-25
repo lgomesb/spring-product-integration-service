8a1edaeb98666a704c4bd965d293aa64b204bc54
leandro <lgomesb@gmail.com>
2024-05-26 00:19:21 +0100

    Open Feign - Add create method

--- src/main/java/com/barbosa/ms/productintegrationservice/productintegrationservice/ProductIntegrationServiceApplication.java
+++ src/main/java/com/barbosa/ms/productintegrationservice/productintegrationservice/ProductIntegrationServiceApplication.java
@@ -1,6 +1,7 @@
 package com.barbosa.ms.productintegrationservice.productintegrationservice;
 
 import com.barbosa.ms.productintegrationservice.productintegrationservice.feign.ProductCatalogMgmtClient;
+import com.barbosa.ms.productintegrationservice.productintegrationservice.feign.dto.request.ProductRequestDTO;
 import com.barbosa.ms.productintegrationservice.productintegrationservice.feign.dto.response.ProductResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -23,7 +24,12 @@ public class ProductIntegrationServiceApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        ProductResponseDTO response = productFeign.getProduct(UUID.fromString("2179d1ef-5c0b-4855-af9b-686192ccc99b"));
+//        ProductResponseDTO response = productFeign.getProduct(UUID.fromString("2179d1ef-5c0b-4855-af9b-686192ccc99b"));
+        ProductRequestDTO request = ProductRequestDTO.builder()
+                .name("teste-01")
+                .idCategory("26dd8bcc-aaba-486d-827a-8ce5933432c0")
+                .build();
+        ProductResponseDTO response = productFeign.create(request);
         System.out.println("::::::::::::::::::: RESPONSE PRODUCT FEIGN CLIENT :::::::::::::::::::");
         System.out.println(response);
     }
