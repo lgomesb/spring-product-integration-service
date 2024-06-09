572c774d1b17dd5131843341ba86b5955656c4f8
leandro <lgomesb@gmail.com>
2024-06-09 18:47:46 +0100

    Resolve merge

--- src/main/java/com/barbosa/ms/productintegrationservice/productintegrationservice/ProductIntegrationServiceApplication.java
+++ src/main/java/com/barbosa/ms/productintegrationservice/productintegrationservice/ProductIntegrationServiceApplication.java
@@ -1,14 +1,8 @@
 package com.barbosa.ms.productintegrationservice.productintegrationservice;
 
-<<<<<<< HEAD
 import com.barbosa.ms.productintegrationservice.productintegrationservice.domain.records.ProductOrderItemRecord;
 import com.barbosa.ms.productintegrationservice.productintegrationservice.domain.records.ProductOrderRequestRecord;
 import com.barbosa.ms.productintegrationservice.productintegrationservice.services.ProductIntegrationServiceService;
-=======
-import com.barbosa.ms.productintegrationservice.productintegrationservice.feign.ProductCatalogMgmtClient;
-import com.barbosa.ms.productintegrationservice.productintegrationservice.feign.dto.request.ProductRequestDTO;
-import com.barbosa.ms.productintegrationservice.productintegrationservice.feign.dto.response.ProductResponseDTO;
->>>>>>> 8a1edaeb98666a704c4bd965d293aa64b204bc54
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -31,7 +25,6 @@ public class ProductIntegrationServiceApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-<<<<<<< HEAD
 
         System.out.println("PRODUCT ORDER");
         ProductOrderRequestRecord productOrderRequestRecord = ProductOrderRequestRecord.builder()
@@ -40,15 +33,5 @@ public class ProductIntegrationServiceApplication implements CommandLineRunner {
                 .build();
         System.out.println(service.createProductOrder(productOrderRequestRecord));
 
-=======
-//        ProductResponseDTO response = productFeign.getProduct(UUID.fromString("2179d1ef-5c0b-4855-af9b-686192ccc99b"));
-        ProductRequestDTO request = ProductRequestDTO.builder()
-                .name("teste-01")
-                .idCategory("26dd8bcc-aaba-486d-827a-8ce5933432c0")
-                .build();
-        ProductResponseDTO response = productFeign.create(request);
-        System.out.println("::::::::::::::::::: RESPONSE PRODUCT FEIGN CLIENT :::::::::::::::::::");
-        System.out.println(response);
->>>>>>> 8a1edaeb98666a704c4bd965d293aa64b204bc54
     }
 }
